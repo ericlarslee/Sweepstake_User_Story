c23ce99e7bfe6d50293b0b2274c167250dac7c06
Eric Lars Lee <ericlarslee@gmail.com>
2021-03-31 13:14:53 -0500

    Current iteration contains framework for project. Next step is to populate sweepstake dictionary

---
+++ contestant.py
@@ -0,0 +1,9 @@
+class Contestant:
+    def __init__(self, fname, lname, email, registration_number):
+        self.first_name = fname
+        self.last_name = lname
+        self.email_address = email
+        self.registration_number = registration_number
+
+    def notify(self):
+        pass
---
+++ sweepstake.py
@@ -0,0 +1,14 @@
+class Sweepstake:
+    def __init__(self, name):
+        self.contestants = {}
+        self.name = name
+
+
+    def register_contestant(self, entry):
+        pass
+
+    def pick_winner(self):
+        pass
+
+    def print_contestant_info(void):
+        pass
